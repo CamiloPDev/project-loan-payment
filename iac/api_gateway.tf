38fd99edeee639d753b7ff7770787839c5bd95b9
CamiloDev <cami20032017@gmail.com>
2024-12-09 18:02:37 -0500

    feat: api methods

--- iac/api_gateway.tf
+++ iac/api_gateway.tf
@@ -26,6 +26,24 @@ resource "aws_api_gateway_resource" "client" {
   path_part = "client"
 }
 
+resource "aws_api_gateway_method" "get_client" {
+  provider = aws.main
+
+  authorization = "NONE"
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.client.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_method" "post_client" {
+  provider = aws.main
+
+  authorization = "NONE"
+  http_method   = "POST"
+  resource_id   = aws_api_gateway_resource.client.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+}
+
 resource "aws_api_gateway_resource" "client_id" {
   provider = aws.main
   
@@ -35,6 +53,32 @@ resource "aws_api_gateway_resource" "client_id" {
   path_part = "{client_id}"
 }
 
+resource "aws_api_gateway_method" "get_client_id" {
+  provider = aws.main
+
+  authorization = "NONE"
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.client_id.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_method" "put_client_id" {
+  provider = aws.main
+
+  authorization = "NONE"
+  http_method   = "PUT"
+  resource_id   = aws_api_gateway_resource.client_id.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_method" "delete_client_id" {
+  provider = aws.main
+
+  authorization = "NONE"
+  http_method   = "DELETE"
+  resource_id   = aws_api_gateway_resource.client_id.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+}
 
 # ---------- Payment ---------- #
 resource "aws_api_gateway_resource" "payment" {
@@ -46,13 +90,22 @@ resource "aws_api_gateway_resource" "payment" {
   path_part = "payment"
 }
 
-resource "aws_api_gateway_resource" "payment_loan_id" {
+resource "aws_api_gateway_method" "get_payment" {
   provider = aws.main
-  
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id = aws_api_gateway_resource.payment.id
 
-  path_part = "{loan_id}"
+  authorization = "NONE"
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.payment.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_method" "post_payment" {
+  provider = aws.main
+
+  authorization = "NONE"
+  http_method   = "POST"
+  resource_id   = aws_api_gateway_resource.payment.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
 }
 
 resource "aws_api_gateway_resource" "payment_id" {
@@ -62,6 +115,42 @@ resource "aws_api_gateway_resource" "payment_id" {
   parent_id = aws_api_gateway_resource.payment.id
 
   path_part = "{payment_id}"
+}
+
+resource "aws_api_gateway_method" "get_payment_id" {
+  provider = aws.main
+
+  authorization = "NONE"
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.payment_id.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_resource" "payment_loan" {
+  provider = aws.main
+  
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id = aws_api_gateway_resource.payment.id
+
+  path_part = "loan"
+}
+
+resource "aws_api_gateway_resource" "payment_loan_id" {
+  provider = aws.main
+  
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id = aws_api_gateway_resource.payment_loan.id
+
+  path_part = "{loan_id}"
+}
+
+resource "aws_api_gateway_method" "get_payment_loan_id" {
+  provider = aws.main
+
+  authorization = "NONE"
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.payment_loan_id.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
 }
 
 # ---------- Loan ---------- #
@@ -74,6 +163,25 @@ resource "aws_api_gateway_resource" "loan" {
   path_part = "loan"
 }
 
+resource "aws_api_gateway_method" "get_loan" {
+  provider = aws.main
+
+  authorization = "NONE"
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.loan.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_method" "post_loan" {
+  provider = aws.main
+
+  authorization = "NONE"
+  http_method   = "POST"
+  resource_id   = aws_api_gateway_resource.loan.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+}
+
+
 resource "aws_api_gateway_resource" "loan_id" {
   provider = aws.main
   
@@ -81,6 +189,33 @@ resource "aws_api_gateway_resource" "loan_id" {
   parent_id = aws_api_gateway_resource.loan.id
 
   path_part = "{loan_id}"
+}
+
+resource "aws_api_gateway_method" "get_loan_id" {
+  provider = aws.main
+
+  authorization = "NONE"
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.loan_id.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_method" "put_loan_id" {
+  provider = aws.main
+
+  authorization = "NONE"
+  http_method   = "PUT"
+  resource_id   = aws_api_gateway_resource.loan_id.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_method" "delete_loan_id" {
+  provider = aws.main
+
+  authorization = "NONE"
+  http_method   = "DELETE"
+  resource_id   = aws_api_gateway_resource.loan_id.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
 }
 
 # ---------- Deploy ---------- #
