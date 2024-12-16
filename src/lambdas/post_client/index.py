d3ea6085fd2cd64ba7f728d9b3444951b61d8a60
CamiloDev <cami20032017@gmail.com>
2024-12-16 11:08:58 -0500

    feat: code post_client

--- src/lambdas/post_client/index.py
+++ src/lambdas/post_client/index.py
@@ -1,20 +1,16 @@
 import os
 import boto3
-import uuid
 import json
 
-
-table_name = os.getenv('TABLE_CLIENT')
+TABLE_CLIENT = os.getenv('TABLE_CLIENT')
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table(table_name)
+table = dynamodb.Table(TABLE_CLIENT)
 
 def handler(event, context):
     try:
-        # Cargar el cuerpo de la solicitud
         body = json.loads(event['body'])
         
-        # Validar los campos requeridos
         required_fields = ['name', 'phoneNumber']
         for field in required_fields:
             if field not in body:
@@ -23,26 +19,20 @@ def handler(event, context):
                     "body": json.dumps({"error": f"El campo '{field}' es obligatorio."})
                 }
         
-        # Generar clientId único
-        client_id = str(uuid.uuid4())
-        
-        # Crear el cliente
-        nuevo_cliente = {
-            'clientId': client_id,
+        new_client = {
+            'clientId': body['clientId'],
             'name': body['name'],
             'phoneNumber': body['phoneNumber']
         }
         
-        # Guardar en DynamoDB
-        table.put_item(Item=nuevo_cliente)
+        table.put_item(Item=new_client)
         
-        # Respuesta exitosa
         return {
             "statusCode": 201,
             "body": json.dumps(
                 {
                     "message": "Cliente creado exitosamente.",
-                    "cliente": nuevo_cliente
+                    "cliente": new_client
                 }
             )
         }
