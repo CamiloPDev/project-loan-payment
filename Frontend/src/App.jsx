0d109d9c76972a440f02c3b1b70162bd918769f2
CamiloDev <cami20032017@gmail.com>
2025-06-14 18:07:04 -0500

    feat: build again react app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,28 +1,35 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import PrestatariosPage from './pages/PrestatariosPage';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-// // function App() {
-// //   return (
-// //     <Router>
-// //       <Routes>
-// //         <Route path="/" element={<PrestatariosPage />} />
-// //       </Routes>
-// //     </Router>
-// //   );
-// // }
-// function App() {
-//   return (
-//     <div className="container mt-5">
-//       <h1>¡Bienvenido al sistema de préstamos!</h1>
-//       <p>Esta es la interfaz inicial.</p>
-//     </div>
-//   )
-// }
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  return <h1 style={{ color: "green", textAlign: "center" }}>¡Hola mundo desde React!</h1>
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
-export default App;
+
+export default App
